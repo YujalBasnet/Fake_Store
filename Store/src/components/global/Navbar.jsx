d3cd564b9711cbe38cd21e5a50ce08ae442e74ea
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-16 15:56:30 +0545

    refactor: Simplify Navbar component structure and remove unused authentication logic

--- Store/src/components/global/Navbar.jsx
+++ Store/src/components/global/Navbar.jsx
@@ -1,44 +1,37 @@
 import { Link } from "react-router-dom";
 import Button from "../Button";
-import { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
-  const { currentUser, logout } = useContext(AuthContext);
-
   return (
     <nav className="flex justify-between px-4 h-20 items-center bg-amber-600">
       <h1 className="text-4xl font-bold">LOGO</h1>
+
       <ul className="flex gap-4">
-        <Link to={"/"}>
-          <li>Home</li>
-        </Link>
-        <Link to={"/about"}>
-          <li>About</li>
-        </Link>
-        <Link to={"/service"}>
-          <li>Service</li>
-        </Link>
-        <Link to={"/contact"}>
-          <li>Contact</li>
-        </Link>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/service">Service</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
       </ul>
 
-      {currentUser ? (
-        <button onClick={() => logout()} className="bg-red-500">
-          Logout
-        </button>
-      ) : (
-        <div className="flex gap-2">
-          <Link to="/login">
-            <Button text="Login" color="blue" />
-          </Link>
-          <Link to="/register">
-            <Button text="Register" color="red" />
-          </Link>
-        </div>
-      )}
+      <div className="flex gap-2">
+        <Link to="/login">
+          <Button text="Login" color="blue" />
+        </Link>
+
+        <Link to="/register">
+          <Button text="Register" color="red" />
+        </Link>
+      </div>
     </nav>
   );
 };
+
 export default Navbar;
