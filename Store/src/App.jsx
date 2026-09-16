c2f65108c5b2e2a778746d71217b43d105b82a49
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-16 15:47:29 +0545

    feat: Implement Homelayout component and configure routing in App component

--- Store/src/App.jsx
+++ Store/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homelayout from './Layout/Homelayout';
 
 const App = () => {
 
   const router =createBrowserRouter([
-    {}
+    {
+      path: "/",
+      element:<Homelayout/>
+    }
   ]);
   return (
     <RouterProvider router={router} />
---
+++ Store/src/Layout/Homelayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homelayout = () => {
+  return (
+    <div>Homelayout</div>
+  )
+}
+
+export default Homelayout
