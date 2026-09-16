70328b9905b552134f8389cf2d67c13e4e992cda
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-16 16:22:18 +0545

    fix: Update Navbar link from About to Products

--- Store/src/components/global/Navbar.jsx
+++ Store/src/components/global/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
         </li>
         <li>
           <Link to="/service">Service</Link>
