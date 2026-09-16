1ebab7d7bf91766b7ba30423673430cfdb2e694c
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-16 18:56:12 +0545

    fix: Correct link structure in Productpage component

--- Store/src/Pages/Productpage.jsx
+++ Store/src/Pages/Productpage.jsx
@@ -31,12 +31,12 @@ const Productpage = () => {
 
       {dataharu.map((product, index) => {
         return (
-          <Link to={`/productsdetails/${product.id}`}>
+          
         <div
           key={product.id}
           className="bg-white rounded-xl shadow-md p-5 hover:shadow-xl transition"
         >
-
+            <Link to={`/productsdetails/${product.id}`}>
           <img
             src={product.image}
             alt={product.title}
@@ -54,21 +54,22 @@ const Productpage = () => {
           <p className="text-sm text-gray-500 mt-2 line-clamp-3">
             {product.description}
           </p>
-
+            </Link>
           <div className="flex justify-between items-center mt-4">
 
             <p className="text-xl font-bold text-green-600">
               ${product.price}
             </p>
-
+          
+            
             <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 cursor-pointer ">
               Add to Cart
             </button>
 
           </div>
-
+            
         </div>
-        </Link>
+        
         );
       })}
       
