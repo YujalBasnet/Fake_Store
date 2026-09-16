bf884f77c77509785a001faf8822f168e0071bd1
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-16 16:25:36 +0545

    feat: Add Productpage component to display products from API

---
+++ Store/src/Pages/Productpage.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const Productpage = () => {
+    const [dataharu, setdataharu] = useState([]);
+    const navigate = useNavigate();
+
+
+    const fetchData = async () => {
+        const dataharu = await axios.get("https://fakestoreapi.com/products");
+        console.log(dataharu.data);
+        setdataharu(dataharu.data);
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+  <div className="min-h-screen bg-gray-100 p-8">
+
+    <h1 className="text-4xl font-bold text-center mb-10">
+      Fake Store Products
+    </h1>
+  
+  <div className="px-4 py-2">
+    <button onClick={() => navigate("/user")}
+            className="bg-black text-white px-7 py-7 rounded-lg cursor-pointer hover:bg-gray-800 transition ">
+            Click here to see the user!!
+        </button>
+  </div>
+
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+
+      {dataharu.map((product, index) => {
+        return (
+          <Link to={`/product_details/${product.id}`}>
+        <div
+          key={product.id}
+          className="bg-white rounded-xl shadow-md p-5 hover:shadow-xl transition"
+        >
+
+          <img
+            src={product.image}
+            alt={product.title}
+            className="w-full h-48 object-contain"
+          />
+
+          <span className="text-xs bg-gray-200 px-3 py-1 rounded-full">
+            Product #{product.id}
+          </span>
+
+          <h2 className="text-lg font-bold mt-4 line-clamp-2">
+            {product.title}
+          </h2>
+
+          <p className="text-sm text-gray-500 mt-2 line-clamp-3">
+            {product.description}
+          </p>
+
+          <div className="flex justify-between items-center mt-4">
+
+            <p className="text-xl font-bold text-green-600">
+              ${product.price}
+            </p>
+
+            <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 cursor-pointer ">
+              Add to Cart
+            </button>
+
+          </div>
+
+        </div>
+        </Link>
+        );
+      })}
+      
+
+    </div>
+  </div>
+);
+};
+
+export default Productpage;
