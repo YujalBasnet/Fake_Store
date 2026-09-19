befb0bc41538ff0cc4f2d5f1a3079a1127a95ac5
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-19 12:05:52 +0545

    feat: Add Cart functionality with CartContext and Cartpage component

--- Store/src/main.jsx
+++ Store/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import "leaflet/dist/leaflet.css";
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { CartContextProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   
     <StrictMode>
       <AuthContextProvider>
+      <CartContextProvider>
       <App />
+      </CartContextProvider>
       </AuthContextProvider>
     </StrictMode>
   
